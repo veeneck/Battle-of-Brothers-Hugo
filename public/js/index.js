a2208126e42d70e0561cf8b3d1524733c11239c8
Ryan Campbell <veeneck@gmail.com>
2018-05-15 16:16:40 -0400

    Comment test

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
 
         var disqusPublicKey = "msqb9E8qDaEGRmXysyMYipQqutxljqNDo4zQmNsql13tmdH1TxqvuGDAvTmYZPG0";
@@ -5,34 +6,31 @@ $(document).ready(function () {
         var urlArray = [];
 
         $('.my-class').each(function () {
-            console.log("looking for comment count");
             var url = $(this).attr('data-disqus-url');
             urlArray.push('link:' + url);
         });
 
 
-        $('#some-button').click(function () {
-            $.ajax({
-               type: 'GET',
-               url: "https://disqus.com/api/3.0/threads/set.jsonp",
-               data: { api_key: disqusPublicKey, forum : disqusShortname, thread : urlArray }, // URL method
-               cache: false,
-               dataType: 'jsonp',
-               success: function (result) {
+        $.ajax({
+           type: 'GET',
+           url: "https://disqus.com/api/3.0/threads/set.jsonp",
+           data: { api_key: disqusPublicKey, forum : disqusShortname, thread : urlArray }, // URL method
+           cache: false,
+           dataType: 'jsonp',
+           success: function (result) {
 
-                    for (var i in result.response) {
+                for (var i in result.response) {
 
-                        var countText = " comments";
-                        var count = result.response[i].posts;
+                    var countText = " comments";
+                    var count = result.response[i].posts;
 
-                        if (count == 1)
-                           countText = " comment";
+                    if (count == 1)
+                       countText = " comment";
 
-                        $('span[data-disqus-url="' + result.response[i].link + '"]').html('' + count + countText);
+                    $('span[data-disqus-url="' + result.response[i].link + '"]').html('' + count + countText);
 
-                    }
                 }
-        });
+            }
       });
 
 });
