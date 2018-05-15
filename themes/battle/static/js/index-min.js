6c0827f037f4e9b9eea16e710d9dc1624b54d10f
Ryan Campbell <veeneck@gmail.com>
2018-05-15 16:03:57 -0400

    testing comment count

---
+++ themes/battle/static/js/index-min.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+        var disqusPublicKey = "msqb9E8qDaEGRmXysyMYipQqutxljqNDo4zQmNsql13tmdH1TxqvuGDAvTmYZPG0";
+        var disqusShortname = "battle-of-brothers";
+        var urlArray = [];
+
+        $('.my-class').each(function () {
+            var url = $(this).attr('data-disqus-url');
+            urlArray.push('link:' + url);
+        });
+
+
+        $('#some-button').click(function () {
+            $.ajax({
+               type: 'GET',
+               url: "https://disqus.com/api/3.0/threads/set.jsonp",
+               data: { api_key: disqusPublicKey, forum : disqusShortname, thread : urlArray }, // URL method
+               cache: false,
+               dataType: 'jsonp',
+               success: function (result) {
+
+                    for (var i in result.response) {
+
+                        var countText = " comments";
+                        var count = result.response[i].posts;
+
+                        if (count == 1)
+                           countText = " comment";
+
+                        $('div[data-disqus-url="' + result.response[i].link + '"]').html('<h4>' + count + countText + '</h4>');
+
+                    }
+                }
+        });
+
+});
+
