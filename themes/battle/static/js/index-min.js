964f4f1fd68493e524c9b2721e62688199fb48c4
Ryan Campbell <veeneck@gmail.com>
2018-05-16 09:02:31 -0400

    Comments

--- themes/battle/static/js/index-min.js
+++ themes/battle/static/js/index-min.js
@@ -1,37 +1,4 @@
-
-$(document).ready(function () {
-
-        var disqusPublicKey = "msqb9E8qDaEGRmXysyMYipQqutxljqNDo4zQmNsql13tmdH1TxqvuGDAvTmYZPG0";
-        var disqusShortname = "battle-of-brothers";
-        var urlArray = [];
-
-        $('.my-class').each(function () {
-            var url = $(this).attr('data-disqus-url');
-            urlArray.push('link:' + url);
-        });
-
-
-        $.ajax({
-           type: 'GET',
-           url: "https://disqus.com/api/3.0/threads/set.jsonp",
-           data: { api_key: disqusPublicKey, forum : disqusShortname, thread : urlArray }, // URL method
-           cache: false,
-           dataType: 'jsonp',
-           success: function (result) {
-
-                for (var i in result.response) {
-
-                    var countText = " comments";
-                    var count = result.response[i].posts;
-
-                    if (count == 1)
-                       countText = " comment";
-
-                    $('span[data-disqus-url="' + result.response[i].link + '"]').html('' + count + countText);
-
-                }
-            }
-      });
-
-});
+function changeValue(elementName, newValue){
+  document.getElementsByName(elementName)[0].value=newValue;
+};
 
